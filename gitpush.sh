61967f52e6190f8c55b452b173e97152628c4ceb
grunclejim <gogartyjames@live.ie>
2016-02-09 23:39:58 +0000

    message

--- gitpush.sh
+++ gitpush.sh
@@ -1,16 +1,4 @@
-#!/bin/bash
-#Program to automate the commiting and pushing of 
-#scripts to github in bash.
-
-
-#asign positional varable 
-
-
 git add .
+git commit -m "message"
+git push -u origin master
 
-git commit -m " " #add in passed in paramater
-
-
-git push origin master
-
-#how to to pass username a password ????
